76e7c537bc2a2b07aa061f5b0162a00330fdf44c
Nau Sakaguchi <nau@NaunoMacBook-Pro.local>
2022-06-12 17:53:50 +0900

    impl step 4

--- src/01/bootload/main.c
+++ src/01/bootload/main.c
@@ -1,5 +1,6 @@
 #include "defines.h"
 #include "serial.h"
+#include "xmodem.h"
 #include "lib.h"
 
 static int init(void)
@@ -20,42 +21,84 @@ static int init(void)
     return 0;
 }
 
-int global_data = 0x10;
-int global_bss;
-static int static_data = 0x20;
-static int static_bss;
-
-static void printval(void)
+/* メモリの16進ダンプ出力 */
+static int dump(char *buf, long size)
 {
-    puts("global_data = ");
-    putxval(global_data, 0);
+    long i;
+
+    if (size < 0)
+    {
+        puts("no data.\n");
+        return -1;
+    }
+    for (i = 0; i < size; i++)
+    {
+        putxval(buf[i], 2);
+        if ((i & 0xf) == 15)
+        {
+            puts("\n");
+        }
+        else
+        {
+            if ((i & 0xf) == 7)
+                puts(" ");
+            puts(" ");
+        }
+    }
     puts("\n");
-    puts("global_bss = ");
-    putxval(global_bss, 0);
-    puts("\n");
-    puts("static_data = ");
-    putxval(static_data, 0);
-    puts("\n");
-    puts("static_bss = ");
-    putxval(static_bss, 0);
-    puts("\n");
+
+    return 0;
+}
+
+static void wait()
+{
+    volatile long i;
+    for (i = 0; i < 300000; i++)
+        ;
 }
 
 int main(void)
 {
-    init();
-    puts("Hello World!\n");
+    static char buf[16];
+    static long size = -1;
+    static unsigned char *loadbuf = NULL;
+    extern int buffer_start; /* リンカ・スクリプトで定義されているバッファ */
 
-    printval();
-    puts("overwrite variables.\n");
-    global_data = 0x20;
-    global_bss = 0x30;
-    static_data = 0x30;
-    static_bss = 0x40;
-    printval();
+    init();
+
+    puts("kzload (kozos boot loader) started.\n");
 
     while (1)
-        ;
+    {
+        puts("kzload> "); /* プロンプト表示 */
+        gets(buf);        /* シリアルからのコマンド受信 */
+
+        if (!strcmp(buf, "load"))
+        {
+            loadbuf = (char *)(&buffer_start);
+            size = xmodem_recv(loadbuf);
+            wait();
+            if (size < 0)
+            {
+                puts("\nXMODEM receive error!\n");
+            }
+            else
+            {
+                puts("\nXMODEM receive succeeded.\n");
+            }
+        }
+        else if (!strcmp(buf, "dump"))
+        {
+            puts("size: ");
+            putxval(size, 0);
+            puts("\n");
+            dump(loadbuf, size);
+        }
+        else
+        {
+            puts("unknown.\n");
+        }
+    }
 
     return 0;
 }
